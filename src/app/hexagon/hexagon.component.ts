fb392e9459e92c91dd9641188694eb6ef59e721e
SimonS97 <simon.schwaldt@hotmail.com>
2023-07-05 14:42:11 +0200

    Anpassung und erstes Styling der Hexagons. Übergabe per Input, Auslagerun der hexagon.component

--- src/app/hexagon/hexagon.component.ts
+++ src/app/hexagon/hexagon.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  AfterViewInit,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import * as PIXI from 'pixi.js';
 
 @Component({
@@ -14,38 +20,56 @@ export class HexagonComponent implements AfterViewInit {
   hexagonSize = 50;
   hexagonWidth = Math.sqrt(3) * this.hexagonSize;
   hexagonHeight = 2 * this.hexagonSize;
+  @Input() hexagonText = '';
+  @Input() hexagonColor!: string;
 
   ngAfterViewInit(): void {
     this.app = new PIXI.Application({
       view: this.pixiCanvas.nativeElement,
-      width: 800,
-      height: 600,
-      // Hintergrundfarbe der Hexagon Komponente
+      width: 100,
+      height: 100,
       backgroundColor: '#ffff',
     });
 
-    // Zeichne ein Hexagon
     const hexagon = new PIXI.Graphics();
-    hexagon.beginFill('#8AB0AB');
-    hexagon.lineStyle(4, '#1A1D1A', 1);
-    hexagon.drawPolygon([
-      -this.hexagonWidth / 2,
-      -this.hexagonHeight / 2,
-      this.hexagonWidth / 2,
-      -this.hexagonHeight / 2,
-      this.hexagonWidth,
+    hexagon.beginFill(parseInt(this.hexagonColor, 16)); // Convert hex color to number
+    hexagon.lineStyle(4, 0x1a1d1a, 1);
+
+    // Calculate the points of the hexagon
+    const points = [
       0,
+      -this.hexagonSize,
       this.hexagonWidth / 2,
-      this.hexagonHeight / 2,
-      -this.hexagonWidth / 2,
-      this.hexagonHeight / 2,
-      -this.hexagonWidth,
+      -this.hexagonSize / 2,
+      this.hexagonWidth / 2,
+      this.hexagonSize / 2,
       0,
-    ]);
+      this.hexagonSize,
+      -this.hexagonWidth / 2,
+      this.hexagonSize / 2,
+      -this.hexagonWidth / 2,
+      -this.hexagonSize / 2,
+    ];
+
+    hexagon.drawPolygon(points);
     hexagon.endFill();
     hexagon.x = this.app.screen.width / 2;
     hexagon.y = this.app.screen.height / 2;
 
-    this.app.stage.addChild(hexagon);
+    const style = new PIXI.TextStyle({
+      fill: '#ffffff',
+      fontSize: 16,
+      fontWeight: 'bold',
+      wordWrap: true,
+      wordWrapWidth: this.hexagonWidth - 20,
+      align: 'center',
+    });
+
+    const text = new PIXI.Text(this.hexagonText, style);
+    text.anchor.set(0.5);
+    text.x = this.app.screen.width / 2;
+    text.y = this.app.screen.height / 2;
+
+    this.app.stage.addChild(hexagon, text);
   }
 }
--- src/app/show-map/show-map.component.ts
+++ src/app/show-map/show-map.component.ts
@@ -6,4 +6,10 @@ import * as PIXI from 'pixi.js';
   templateUrl: './show-map.component.html',
   styleUrls: ['./show-map.component.css'],
 })
-export class ShowMapComponent {}
+export class ShowMapComponent {
+  hexagonsData = [
+    { text: 'Hexagon 1', color: '26413C' },
+    { text: 'Hexagon 2', color: '1A1D1A' },
+    // Weitere Hexagon-Daten hier...
+  ];
+}
