659f006596ff5ca17d242420b1a7f68bb3d8a2be
SimonS97 <simon.schwaldt@hotmail.com>
2023-07-05 12:34:13 +0200

    Zeichnen eines ersten Polygons / Hexagon mithilfe von Pixi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { HomeComponent } from './home/home.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ShowMapComponent } from './show-map/show-map.component';
 
 @NgModule({
@@ -15,6 +19,10 @@ import { ShowMapComponent } from './show-map/show-map.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
