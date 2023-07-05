8af45419a5fcd406a6fad4f5a359c2d3ca4c2d92
SimonS97 <simon.schwaldt@hotmail.com>
2023-07-05 10:37:30 +0200

    Anlage der Base Componenten + Material Design

---
+++ src/app/show-map/show-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-map',
+  templateUrl: './show-map.component.html',
+  styleUrls: ['./show-map.component.css']
+})
+export class ShowMapComponent {
+
+}
