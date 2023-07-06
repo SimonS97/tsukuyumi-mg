d7e2572617b06dce3a2867a8c8228df24861de61
SimonS97 <simon.schwaldt@hotmail.com>
2023-07-06 11:27:31 +0200

    Hexagons werden mit Text generiert.

--- src/app/show-map/show-map.component.ts
+++ src/app/show-map/show-map.component.ts
@@ -12,8 +12,10 @@ export class ShowMapComponent implements AfterViewInit {
   pixiCanvas!: ElementRef<HTMLCanvasElement>;
   hexagonSize = 30;
   hexagonOrigin = 'topLeft'; // 'center' if you want the center to be the origin
-  gridWidth = 10;
-  gridHeight = 10;
+  // Tatsächlichen Dimensionen des Grids
+  // gridWidth * gridHeight = Anzahl Hexagons sollte immer 28 sein
+  gridWidth = 5;
+  gridHeight = 6;
 
   ngAfterViewInit(): void {
     const Hex = defineHex({
@@ -32,12 +34,35 @@ export class ShowMapComponent implements AfterViewInit {
     const graphics = new PIXI.Graphics();
     app.stage.addChild(graphics);
 
+    // Hiermit Hexagons Ausblende + Text #fffff
     graphics.lineStyle(1, 0x999999);
+    let counter = 0;
 
-    grid.forEach((hex: any) => renderHex(hex, graphics));
+    grid.forEach((hex: any) => {
+      const newText = renderHex(hex, graphics);
+      app.stage.addChild(newText);
+      counter++;
+    });
+    console.log(counter);
   }
 }
 
-function renderHex(hex: any, graphics: PIXI.Graphics) {
+function renderHex(hex: any, graphics: PIXI.Graphics): PIXI.Text {
   graphics.drawShape(new PIXI.Polygon(hex.corners));
+
+  const textStyle = new PIXI.TextStyle({
+    fill: '#000000',
+    fontSize: 12,
+    fontWeight: 'bold',
+    wordWrap: true,
+    wordWrapWidth: 10,
+    align: 'center',
+  });
+
+  const text = new PIXI.Text('Test', textStyle);
+  text.anchor.set(0.5);
+  text.x = hex.x;
+  text.y = hex.y;
+
+  return text;
 }
